cb2849051f2d0d45acc3760ecf162f0cc3baefb0
Marc Heuker of Hoek <me@marchoh.com>
2016-11-28 12:17:30 +0100

    changed file paths to latin1

--- src/mainwindow/mainwindow.cpp
+++ src/mainwindow/mainwindow.cpp
@@ -178,8 +178,8 @@ void MainWindow::exportFiles()
 void MainWindow::initializeVault(const QString vaultPath, const QString keyStorePath)
 {
     // Convert names
-    const char *cVaultPath = vaultPath.toStdString().c_str();
-    const char *cKeyStorePath = keyStorePath.toStdString().c_str();
+    const char *cVaultPath = vaultPath.toLatin1().data();
+    const char *cKeyStorePath = keyStorePath.toLatin1().data();
 
     // Setup vault
     psInstance->init(cVaultPath, cKeyStorePath);
