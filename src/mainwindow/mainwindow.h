e7e9ab8f2d5344382526746db07c2f38e47c06dd
Marc Heuker of Hoek <me@marchoh.com>
2016-12-13 13:49:05 +0100

    We can now also edit files that lie in sub directories

--- src/mainwindow/mainwindow.h
+++ src/mainwindow/mainwindow.h
@@ -46,6 +46,7 @@ private:
     PartitionSafe* psInstance;
     QModelIndexList selectedRowsList;
     QFileSystemWatcher* watcher;
+    QHash<QString, Entry*> modifiedFileList;
 
     void setPath();
     void importFiles();
